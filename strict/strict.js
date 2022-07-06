707fa0231442d3e9e20d99dada7f60033dfb6b23
Mohammad-Nikkhah <mohammadnikan47@gmail.com>
2022-07-06 23:54:10 +0430

    finish day 1

---
+++ strict/strict.js
@@ -0,0 +1,25 @@
+// function foo() {
+//     "use strict"
+//     // this code is strict mode 
+//     function bar() {
+//         // this code is strict mode 
+//     }
+// }
+
+// {
+//     "use strict"
+//     // this code in strict mode 
+// }
+
+
+// // example  strict mode 
+
+// function test() {
+//     "use strict" // trun on strict mode 
+//     //  data =1; // var missing Refrenceerror
+// } 
+
+// test()
+
+// // Rules in strict mode in
+

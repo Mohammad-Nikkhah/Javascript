1ebb7bb07baf107858059e2c8c36c48dfc4e019c
Mohammad-Nikkhah <mohammadnikan47@gmail.com>
2022-07-06 22:53:09 +0430

    finish hoisting

--- Hoisting/hoisting.js
+++ Hoisting/hoisting.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+// declare variable by hoisting
+count = 15 ;
+if (count > 10) {
+    console.log("Great")
+}
+var count;
+
+// hoisting in function 
+
+// use welcome function before declare
+
+welcome("Mohammad-nikkha");
+
+function welcome(username) {
+    console.log(`hi ${username}`)
+} 
+
+
